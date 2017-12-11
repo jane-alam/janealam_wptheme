b5b014e230d5e2612eeea313ab2207e13535b79c
Mahammad Jan-E-Alam <janealam416@gmail.com>
2017-12-11 11:16:53 +0600

    Create janealam_wptheme.php

---
+++ janealam_wptheme.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Janealam Wptheme
+ *
+ * @package   janealam_wptheme
+ * @author    Jane Alam
+ * @license   GPL-2.0+
+ * @link      https://github.com/jane-alam/janealam_wptheme
+ */
+
+/**
+ * Plugin Name:       Janealam Wptheme
+ * Plugin URI:        https://github.com/jane-alam/janealam_wptheme
+ * Description:       A plugin to automatically update GitHub, Bitbucket, or GitLab hosted plugins, themes, and language packs. It also allows for remote installation of plugins or themes into WordPress.
+ * Version:           0.0.1
+ * Author:            Andy Fragen
+ * License:           GNU General Public License v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * Domain Path:       /languages
+ * Text Domain:       janealam-wptheme
+ * Network:           true
+ * GitHub Plugin URI: https://github.com/jane-alam/janealam_wptheme
+ * Requires WP:       4.6
+ * Requires PHP:      5.3
+ */
